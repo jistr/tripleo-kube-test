e99e1dc00242140ea38555094468bf3e03ab49f0
Jiri Stransky <jistr@redhat.com>
2017-01-12 12:35:52 +0100

    Pre-create multiple PVs, incl. the NFS exports

--- infra/host-nfs-pv/nfs-pv-export.sh
+++ infra/host-nfs-pv/nfs-pv-export.sh
@@ -7,10 +7,12 @@ sudo systemctl start rpcbind
 sudo systemctl start nfs-server
 sudo setsebool -P nfs_export_all_rw 1
 
-sudo mkdir -p /export/kubernetes-pv
-if ! grep /export/kubernetes-pv /etc/exports; then
-    echo "/export/kubernetes-pv *(rw,sync,all_squash)" | sudo tee -a /etc/exports
-fi
+for EXPORT_NUM in $(seq 0 9); do
+    sudo mkdir -p /export/kubernetes-pv-$EXPORT_NUM
+    if ! grep /export/kubernetes-pv-$EXPORT_NUM /etc/exports; then
+        echo "/export/kubernetes-pv-$EXPORT_NUM *(rw,sync,all_squash)" | sudo tee -a /etc/exports
+    fi
+done
 
 sudo chown nfsnobody:nfsnobody /export/kubernetes-pv
 sudo exportfs -rav

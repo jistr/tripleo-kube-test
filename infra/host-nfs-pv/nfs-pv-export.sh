fedacf8130053bcba020bf327a5b4d530715acbb
Jiri Stransky <jistr@redhat.com>
2017-01-12 13:28:00 +0100

    Use /etc/exports.d/kubernetes-pv instead of /etc/exports
    Safer w/r/t avoiding collisions with other exports when editing the
    file.

--- infra/host-nfs-pv/nfs-pv-export.sh
+++ infra/host-nfs-pv/nfs-pv-export.sh
@@ -7,10 +7,11 @@ sudo systemctl start rpcbind
 sudo systemctl start nfs-server
 sudo setsebool -P nfs_export_all_rw 1
 
+sudo truncate -s 0 /etc/exports.d/kubernetes-pv.exports
 for EXPORT_NUM in $(seq 0 9); do
     sudo mkdir -p /export/kubernetes-pv-$EXPORT_NUM
     if ! grep /export/kubernetes-pv-$EXPORT_NUM /etc/exports; then
-        echo "/export/kubernetes-pv-$EXPORT_NUM *(rw,sync,all_squash)" | sudo tee -a /etc/exports
+        echo "/export/kubernetes-pv-$EXPORT_NUM *(rw,sync,all_squash)" | sudo tee -a /etc/exports.d/kubernetes-pv.exports
     fi
 done
 

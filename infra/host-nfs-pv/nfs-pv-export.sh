c16dc04f8c5d2eb8f3c9b978d5a562496f4971ea
Jiri Stransky <jistr@redhat.com>
2017-01-12 14:57:17 +0100

    More generally usable NFS permissions setup
    Mysql load would fail with:
    
    chown: changing ownership of '/var/lib/mysql': Operation not permitted
    
    Changing the permisssions according to
    
    https://docs.openshift.com/enterprise/3.0/admin_guide/persistent_storage_nfs.html
    
    didn't fully help either -- this scenario assumes that the application
    will be ok with simply reading/writing its data and not attempt
    `chown`ing it. For kolla we actually need no_root_squash to map the
    uid/gid of root to the NFS host, to allow doing chown.

--- infra/host-nfs-pv/nfs-pv-export.sh
+++ infra/host-nfs-pv/nfs-pv-export.sh
@@ -6,14 +6,16 @@ sudo yum -y install nfs-utils
 sudo systemctl start rpcbind
 sudo systemctl start nfs-server
 sudo setsebool -P nfs_export_all_rw 1
+sudo setsebool -P virt_use_nfs 1
 
 sudo truncate -s 0 /etc/exports.d/kubernetes-pv.exports
 for EXPORT_NUM in $(seq 0 9); do
     sudo mkdir -p /export/kubernetes-pv-$EXPORT_NUM
     if ! grep /export/kubernetes-pv-$EXPORT_NUM /etc/exports; then
-        echo "/export/kubernetes-pv-$EXPORT_NUM *(rw,sync,all_squash)" | sudo tee -a /etc/exports.d/kubernetes-pv.exports
+        echo "/export/kubernetes-pv-$EXPORT_NUM *(rw,sync,no_root_squash)" | sudo tee -a /etc/exports.d/kubernetes-pv.exports
     fi
     sudo chown nfsnobody:nfsnobody /export/kubernetes-pv-$EXPORT_NUM
+    sudo chmod -R a+rwX /export/kubernetes-pv-$EXPORT_NUM
 done
 
 sudo exportfs -rav

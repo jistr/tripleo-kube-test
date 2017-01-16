61faca5c36afe254a0aa4b241671a0a231d33db6
Jiri Stransky <jistr@redhat.com>
2017-01-16 13:43:58 +0100

    Add rabbitmq to the create/remove scripts

--- create.sh
+++ create.sh
@@ -3,18 +3,26 @@
 
 set -euxo pipefail
 
-SERVICES="create_mysql"
+SERVICES="create_mysql create_rabbitmq"
 
 create_mysql() {
-   kubectl create -f services/mysql/configmap.yaml
-   kubectl create -f services/mysql/service.yaml
-   kubectl create -f services/mysql/statefulset.yaml -f services/mysql/bootstrap-job.yml
+  kubectl create -f services/mysql/configmap.yaml
+  kubectl create -f services/mysql/service.yaml
+  kubectl create -f services/mysql/statefulset.yaml -f services/mysql/bootstrap-job.yml
 }
 
+create_rabbitmq() {
+  kubectl create -f services/rabbitmq/configmap.yml
+  kubectl create -f services/rabbitmq/service.yml
+  kubectl create -f services/rabbitmq/statefulset.yml
+}
 
 case "${1:-all}" in
   mysql)
     SERVICES="create_mysql"
+  ;;
+  rabbitmq)
+    SERVICES="create_rabbitmq"
   ;;
   *)
   ;;
--- remove.sh
+++ remove.sh
@@ -3,19 +3,27 @@
 
 set -euxo pipefail
 
-SERVICES="create_mysql"
+SERVICES="delete_mysql delete_rabbitmq delete_all_pvc"
 
-create_mysql() {
-   kubectl delete statefulset mysql || true 
-   kubectl delete job mysql-bootstrap || true 
-   kubectl delete service mysql || true
-   kubectl delete configmap mariadb-kolla-config || true
+delete_mysql() {
+  kubectl delete statefulset mysql || true
+  kubectl delete job mysql-bootstrap || true
+  kubectl delete service mysql || true
+  kubectl delete configmap mariadb-kolla-config || true
 }
 
+delete_rabbitmq() {
+  kubectl delete statefulset rabbitmq || true
+  kubectl delete service rabbitmq || true
+  kubectl delete configmap rabbitmq-kolla-config || true
+}
 
 case "${1:-all}" in
   mysql)
-    SERVICES="create_mysql"
+    SERVICES="delete_mysql"
+  ;;
+  rabbitmq)
+    SERVICES="delete_rabbitmq"
   ;;
   *)
   ;;

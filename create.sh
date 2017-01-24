d492c24c05ea1adcafdd0f009ebda2c33f25ee9b
Jiri Stransky <jistr@redhat.com>
2017-01-24 15:19:29 +0100

    Introduce wait_for_job function, use it for mysql-bootstrap

--- create.sh
+++ create.sh
@@ -5,10 +5,32 @@ set -euxo pipefail
 
 SERVICES="create_mysql create_rabbitmq create_glance create_keystone"
 
+wait_for_job() {
+    SLEEP=5
+    JOB_NAME="$1"
+    TIMEOUT="${2:-120}"  # approximate
+
+    TRY=0
+    MAX_TRIES=$(( $TIMEOUT / $SLEEP ))
+    while [ $TRY -lt $MAX_TRIES ]; do
+        TRY=$(( $TRY + 1 ))
+
+        if kubectl get jobs -o=jsonpath='{.items[?(@.status.succeeded)].metadata.name}' | grep "\<$JOB_NAME\>"; then
+            return 0
+        fi
+
+        sleep $SLEEP
+    done
+
+    echo "Timed out."
+    exit 1
+}
+
 create_mysql() {
   kubectl create -f services/mysql/configmap.yaml
   kubectl create -f services/mysql/service.yaml
   kubectl create -f services/mysql/statefulset.yaml -f services/mysql/bootstrap-job.yaml
+  wait_for_job mysql-bootstrap
 }
 
 create_rabbitmq() {
